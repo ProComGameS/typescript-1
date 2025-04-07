bc1cabae526fca9dcd09d29329f2f6c230482d6d
ProComGameS <sas2005ass@gmail.com>
2025-04-07 18:10:04 +0300

    Third task

--- main.ts
+++ main.ts
@@ -71,16 +71,27 @@ function createUser(name: string, age: number, active: boolean): User {
  *
  */
 
-enum OrderStatus {}
+enum OrderStatus {Pending, Shipped, Delivered, Cancelled}
 
-function getOrderStatus() {
-  // code here
+function getOrderStatus(status: OrderStatus): string {
+    switch (status) {
+        case OrderStatus.Pending:
+            return 'Замовлення очікує на обробку'
+        case OrderStatus.Delivered:
+            return 'Замовлення доставлено'
+        case OrderStatus.Shipped:
+            return 'Замовлення було відправлено'
+        case OrderStatus.Cancelled:
+            return 'Замовлення скасовано'
+        default:
+            throw new Error('Невідомий статус замовлення')
+    }
 }
 
 // Приклад виклику функції
-// console.log(getOrderStatus(OrderStatus.Pending))
-// console.log(getOrderStatus(OrderStatus.Shipped))
-// console.log(getOrderStatus(OrderStatus.Delivered))
-// console.log(getOrderStatus(OrderStatus.Cancelled))
+ console.log(getOrderStatus(OrderStatus.Pending))
+ console.log(getOrderStatus(OrderStatus.Shipped))
+ console.log(getOrderStatus(OrderStatus.Delivered))
+ console.log(getOrderStatus(OrderStatus.Cancelled))
 
 export { sumArray, createUser, OrderStatus, getOrderStatus }
